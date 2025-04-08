0d0cc14d3da47c9908b3155539cbd99afd8cf035
Theodor Negrescu <theodor.negrescu@gmail.com>
2025-04-08 15:59:04 +0300

    Add basic sphere drawing

--- xsort_subproc.c
+++ xsort_subproc.c
@@ -1,11 +1,43 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
+#include <inttypes.h>
+#include <string.h>
 
 #include <unistd.h>
 #include <X11/Xlib.h>
 
+void draw_num_sphere(Display *display, Window window, GC gc, XFontStruct font, int centerX, int centerY, int radius, const char *numStr) {
+    int numWidth = XTextWidth(&font, numStr, strlen(numStr));
+    int numHeight = font.ascent + font.descent;
+    int x = centerX - numWidth / 2;
+    int y = centerY - numHeight / 2;
+    XDrawArc(display, window, gc, centerX - radius, centerY - radius, 2 * radius, 2 * radius, 0, 360 * 64);
+    XDrawString(display, window, gc, x, y + font.ascent, numStr, strlen(numStr));
+}
+
 void run_sort(int64_t *buf, int bufLen, int actionIdx) {
+    char **numStr = reallocarray(NULL, bufLen, sizeof(char*));
+    char *strBuf = reallocarray(NULL, bufLen, 22);
+    char *bufPtr = strBuf;
+    if(!numStr) {
+        perror("reallocarray");
+        exit(1);
+    }
+    if(!strBuf) {
+        perror("reallocarray");
+        exit(1);
+    }
+    for(int i = 0; i < bufLen; i++) {
+        numStr[i] = bufPtr;
+        int len = sprintf(bufPtr, "%" PRId64, buf[i]);
+        if(len < 0) {
+            perror("sprintf");
+            exit(1);
+        }
+        bufPtr += len + 1;
+    }
+
     Display *display = XOpenDisplay(NULL);
     int blackColor = BlackPixel(display, DefaultScreen(display));
     int whiteColor = WhitePixel(display, DefaultScreen(display));
@@ -17,7 +49,18 @@ void run_sort(int64_t *buf, int bufLen, int actionIdx) {
         exit(1);
     }
 
-    Window window = XCreateSimpleWindow(display, DefaultRootWindow(display), 0, 0, 400, 400, 0, blackColor, whiteColor);
+    int maxWidth = 0;
+    for(int i = 0; i < bufLen; i++) {
+        int width = XTextWidth(font, numStr[i], strlen(numStr[i]));
+        if(width > maxWidth) {
+            maxWidth = width;
+        }
+    }
+    int radius = maxWidth / 2 + 5;
+
+    int windowHeight = radius * 2 + 10 + font->ascent + font->descent + 10;
+
+    Window window = XCreateSimpleWindow(display, DefaultRootWindow(display), 0, 0, 400, windowHeight, 0, blackColor, whiteColor);
     XSelectInput(display, window, StructureNotifyMask);
     XMapWindow(display, window);
 
@@ -33,12 +76,19 @@ void run_sort(int64_t *buf, int bufLen, int actionIdx) {
         }
     }
 
-    XDrawString(display, window, gc, 10, 20, "TODO", 4);
+    int x = 10 + radius;
+    int y = 10 + radius;
+    for(int i = 0; i < bufLen; i++) {
+        draw_num_sphere(display, window, gc, *font, x, y, radius, numStr[i]);
+        x += radius * 2 + 10;
+    }
     XFlush(display);
-    sleep(2);
+    sleep(5);
 
     XFreeGC(display, gc);
     XFreeFont(display, font);
     XDestroyWindow(display, window);
     XCloseDisplay(display);
+    free(numStr);
+    free(strBuf);
 }
